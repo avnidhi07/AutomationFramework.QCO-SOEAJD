e4e40304e6466fc0671042a7081a6b8d3b628c6e
avni <gavnish.91194@gmail.com>
2023-09-25 14:13:00 +0530

    3rd Commit and Push

--- src/test/java/vtiger/ProductsTest/CreateProductWithCampaignTest.java
+++ src/test/java/vtiger/ProductsTest/CreateProductWithCampaignTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import Generic_Utilities.BaseClass;
 import Generic_Utilities.ExcelUtility;
 import Generic_Utilities.JavaUtility;
+import POMRepository.CampaignPage;
+import POMRepository.CreateCampaignPage;
 import POMRepository.CreateProductPage;
 import POMRepository.HomePage;
 import POMRepository.ProductsPage;
@@ -18,6 +20,7 @@ public class CreateProductWithCampaignTest extends BaseClass {
 	public void createProductWithCampaignTest() throws Throwable { //https://docs.google.com/forms/d/1Eorr4JdbQbZctvvWh62-c8QIAw-6SPYY_d-uHjlB8vI/viewform?edit_requested=true
 		
 		String PrName = eUtil.getExcelDataUsingDataFormatter("Products", 0, 0)+jUtil.getRandomNum();
+		String CampName = eUtil.getExcelDataUsingDataFormatter("Campaign", 0, 0)+jUtil.getRandomNum();
 		
 		HomePage hp=new HomePage(driver);
 		hp.clickProductsLnk();
@@ -31,7 +34,11 @@ public class CreateProductWithCampaignTest extends BaseClass {
 		hp.moveTomore(driver);
 		hp.clickCampaign();
 		
+		CampaignPage cp=new CampaignPage(driver);
+		cp.createCampaign();
 		
+		CreateCampaignPage ccp=new CreateCampaignPage(driver);
+		ccp.createNewCampaign(CampName, driver, PrName);
 		
 		
 		
